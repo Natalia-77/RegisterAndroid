5cc314bd4a4b7b559b372803e2d226918e3a9dc5
vlasnata80@gmail.com <vlasnata80@gmail.com>
2022-04-04 18:25:48 +0300

    Add launcher

--- app/src/main/java/com/example/register/MainActivity.java
+++ app/src/main/java/com/example/register/MainActivity.java
@@ -2,7 +2,13 @@ package com.example.register;
 
 
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
@@ -35,6 +41,29 @@ public class MainActivity extends AppCompatActivity {
     static final String USER_KEY = "USER";
     String selectPhoto="";
 
+//
+    ActivityResultLauncher<Intent> startResult = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        Intent intent = result.getData();
+                            Uri uri = intent.getData();
+                            image.setImageURI(uri);
+                            try {
+                                bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(),uri);
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+
+                            ByteArrayOutputStream stream=new ByteArrayOutputStream();
+                            bitmap.compress(Bitmap.CompressFormat.JPEG,100,stream);
+                            byte[] bytes=stream.toByteArray();
+                            selectPhoto= Base64.encodeToString(bytes,Base64.DEFAULT);
+                        }
+                }
+           });
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,32 +84,8 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent();
         intent.setType("image/*");
         intent.setAction(Intent.ACTION_GET_CONTENT);
-        startActivityForResult(Intent.createChooser(intent, "Select Picture"), 200);
-    }
+        startResult.launch(Intent.createChooser(intent, "Select Image"));
 
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if (resultCode == RESULT_OK) {
-
-            if (requestCode == 200) {
-
-                Uri uri = data.getData();
-                image.setImageURI(uri);
-
-                try {
-                    bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(),uri);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-
-                ByteArrayOutputStream stream=new ByteArrayOutputStream();
-                bitmap.compress(Bitmap.CompressFormat.JPEG,100,stream);
-                byte[] bytes=stream.toByteArray();
-                selectPhoto= Base64.encodeToString(bytes,Base64.DEFAULT);
-            }
-
-        }
     }
 
 
